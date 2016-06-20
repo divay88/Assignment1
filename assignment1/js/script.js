371704f1c4cfc82fb612d8a4a2ea09456438ea4a
divay88 <divay.chaudhri@gmail.com>
2016-06-20 13:33:16 -0700

    Add files via upload

---
+++ assignment1/js/script.js
@@ -0,0 +1,231 @@
+//Divay
+
+//STEP 1
+/*
+var someMonth;
+function theMonth();  //function to return current month
+curentMonth           // a constant
+var summerMonth;       // an array of summer months
+myLibraryAFunction();  // a function
+*/
+
+//STEP 2
+/*  
+1234                    //numberic
+"Clevland Caveliers"    //string
+true                    //boolean
+null                    //null
+*/
+
+//STEP 3
+/* 
+var x = 2 - 10;
+var y = x / 4;
+*/
+
+//STEP 4
+/*
+var firstName
+var lastName
+var txtAddress
+var strCity
+var state
+var intZip
+var intYourAge
+var strReferalSource
+var strMayWeContactYou
+*/
+
+//STEP 5
+/*
+var firstName = 'Dee';
+var lastName = prompt("James");
+var intZip = '12345';
+/*
+
+//STEP 6
+/*
+var myName = "Irving";
+    console.log("My name is " + myName + .");
+    
+
+var win = false;
+    console.log(Results? + win +);
+    
+var lost = true:
+    console.log(1 + lost);
+*/
+
+//STEP 7 
+/*
+var x = 1;
+    console.log("x is " + x + " and y is " + y ".");
+var y = 5; 
+*/
+
+//STEP 8
+
+/* It is not valid.  I would fix it by adding escape charachters in front of special characters. 
+
+var someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former.\"';
+console.log(someString);
+*/
+
+// STEP 9 
+// Create a variable that produces a null value in the console window. Also, create a variable that produces an undefined value in the console window.
+/*
+var x= null;
+    console.log(x);
+
+var y;
+    console.log(y);
+*/
+
+// STEP 10
+// Use the typeof operator on various literals to return the following types within the console window: string, number, Boolean, object, and undefined.
+/*
+var x = "Bob";
+    console.log(typeof(x));
+
+var y = 888;
+    console.log(typeof(y));
+
+var z = true;
+    console.log(typeof(z));
+
+var person = ['john' , 'phil', 'steve'];
+    console.log(typeof(person));
+
+var q;
+    console.log(typeof(q));
+*/
+
+// STEP !!
+/* 
+var myName = "Dee";
+    alert('Hello ' + myName + ', welcome to the JavaScript class!');
+*/
+
+//STEP 12
+/*
+var name = 'Dhevay Singh';
+    alert('Hello ' + name + ', welcome to the JavaScript class!');
+*/
+
+// STEP 13
+/*
+var course = 'JavaScript';
+    alert('Hello ' + name + ', welcome to the ' + course +  ' class!');
+*/
+
+// STEP 14
+/*
+var course = "JavaScript"
+     alert('Hello ' + name + '. \nWelcome to the ' + course +  ' class!');
+*/
+
+// STEP 15
+/*
+var name = prompt('What is your name?');
+    alert(name);
+*/
+
+// STEP 16
+/* var course = prompt("What class are you taking?");
+    alert(course);
+*/
+
+// STEP 17
+/*
+var x = 10;
+var y = 20;
+    console.log(x+y);
+*/
+
+// STEP 18
+/*
+var x = 20;
+    console.log(x+20);
+*/
+
+// STEP 19
+/*
+var x = 20;
+    console.log(x*5);
+*/
+
+// STEP 20
+/*
+var x = 20 % 3;
+    console.log(x / 1);
+*/
+
+// STEP 21
+/*
+var x = 8;
+var y = true;
+    console.log(x > 1 || y);
+*/
+
+//STEP 22
+/*
+var x = 12345;
+var y = 123;
+    console.log(x < y);
+*/
+
+//STEP 23
+/*
+var widget = new Object();
+    console.log(typeof(widget));
+*/
+
+//STEP 24
+/*
+var widget = new Object();
+    console.log(widget instanceof Object);
+*/
+
+// STEP 25
+/*
+var widget = new Object();
+    console.log(widget instanceof Object);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
